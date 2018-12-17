dce322c6dd279ec2173492e0d285bc29998da026
Putzi San <putzisan@putzisan.de>
2018-12-17 11:32:49 +0100

    add code-splitting for vendors

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const SizePlugin = require('size-plugin');
+const pgkJson = require('./package.json');
 
 require('dotenv').config();
 
@@ -15,11 +17,33 @@ const cssLoader = require.resolve('css-loader');
 
 const isDev = process.env.NODE_ENV === 'development';
 
+const vendors = Object.keys(pgkJson.dependencies);
+
+const vendorCacheGroups = {};
+
+vendors.forEach(vendor => {
+  vendorCacheGroups[vendor] = {
+    test: new RegExp(`[\\\\/]node_modules[\\\\/]${vendor}[\\\\/]`),
+    chunks: 'initial',
+    name: vendor,
+    enforce: true,
+  };
+});
+
+const otherVendors = {
+  test: new RegExp(
+    `[\\\\/]node_modules[\\\\/](?!(?:${vendors.join('|')})).*[\\\\/]`
+  ),
+  chunks: 'initial',
+  name: 'vendors',
+  enforce: true,
+};
+
 module.exports = {
   entry: [path.join(__dirname, 'src', 'index.tsx')],
   output: {
     filename: isDev ? 'js/[name].js' : 'js/[name].[contenthash:8].js',
-    chunkFilename: isDev ? 'js/[id].js' : 'js/[id].[contenthash:8].js',
+    chunkFilename: isDev ? 'js/[id].js' : 'js/[name].[contenthash:8].js',
     path: path.join(__dirname, '/dist'),
     publicPath: '/',
   },
@@ -64,13 +88,19 @@ module.exports = {
       new UglifyJsPlugin(require('./dev/webpack/ugilfyJsPluginOptions')),
       new OptimizeCSSAssetsPlugin(require('./dev/webpack/optimizeCssOpts')),
     ],
+
+    splitChunks: {
+      minSize: 5000,
+      // cacheGroups: Object.assign({}, vendorCacheGroups, { otherVendors }),
+      cacheGroups: Object.assign({}, vendorCacheGroups, { otherVendors }),
+    },
     // tested automatic vendor-splitting via splitChunks [optimization.splitChunks](https://webpack.js.org/plugins/split-chunks-plugin/#splitchunks-maxasyncrequests)
     // but its currently not that good (explicit code-splitting may be ebther ATM)
     // good is `maxSize` https://webpack.js.org/plugins/split-chunks-plugin/#splitchunks-maxsize necessary with http/2, but currently it causes random bugs for real-world-app (apoly)
 
     // https://twitter.com/wSokra/status/969679223278505985 + https://webpack.js.org/configuration/optimization/#optimization-runtimechunk
     // https://twitter.com/wSokra/status/969679223278505985 + https://webpack.js.org/configuration/optimization/#optimization-runtimechunk
-    runtimeChunk: true, // => runtimeChunk for longTermCaching so js-files have not to include the other chunks and not changed chunks could use the cached var
+    runtimeChunk: 'single', // => runtimeChunk for longTermCaching so js-files have not to include the other chunks and not changed chunks could use the cached var
   },
   plugins: [
     new webpack.EnvironmentPlugin(Object.keys(process.env)),
@@ -81,5 +111,6 @@ module.exports = {
     }),
     isDev && new webpack.HotModuleReplacementPlugin(),
     !isDev && new MiniCssExtractPlugin(require('./dev/webpack/cssExtractOpts')),
+    !isDev && new SizePlugin(),
   ].filter(Boolean),
 };
